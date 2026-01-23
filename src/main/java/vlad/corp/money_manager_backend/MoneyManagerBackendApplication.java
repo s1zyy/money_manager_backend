a2ddcc8e9e319834b92c657bb13031eec4405112
Vlad <sav320801@gmail.com>
2026-01-23 18:49:11 +0000

    innit commit

---
+++ src/main/java/vlad/corp/money_manager_backend/MoneyManagerBackendApplication.java
@@ -0,0 +1,13 @@
+package vlad.corp.money_manager_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneyManagerBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoneyManagerBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/vlad/corp/money_manager_backend/MoneyManagerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package vlad.corp.money_manager_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneyManagerBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
