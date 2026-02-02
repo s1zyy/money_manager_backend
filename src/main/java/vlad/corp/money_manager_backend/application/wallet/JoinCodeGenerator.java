00ad11f8ce7be6b77044d1567074fb6b98ed5432
Vlad <sav320801@gmail.com>
2026-02-02 15:51:52 +0000

    chore: remove old waller, user and transaction modules

--- src/main/java/vlad/corp/money_manager_backend/application/wallet/JoinCodeGenerator.java
+++
@@ -1,7 +0,0 @@
-package vlad.corp.money_manager_backend.application.wallet;
-
-import vlad.corp.money_manager_backend.domain.model.JoinCode;
-
-public interface JoinCodeGenerator {
-    JoinCode generate();
-}
