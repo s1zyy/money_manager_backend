c665ca100b6f217bd37084859789c6751fa6baf1
Vlad <sav320801@gmail.com>
2026-02-12 19:58:26 +0000

    fixup

---
+++ src/main/java/vlad/corp/money_manager_backend/presentation/dto/expense/CreateExpenseDto.java
@@ -0,0 +1,13 @@
+package vlad.corp.money_manager_backend.presentation.dto.expense;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.Set;
+import java.util.UUID;
+
+public record CreateExpenseDto(
+        BigDecimal amount,
+        LocalDate date,
+        Set<UUID> participantIds,
+        String description
+) {}
